7453ba05fde32902f880176c0361fd9b3f15f4aa
RodrigoBergerDev <bergerrd@gmail.com>
2025-01-15 01:01:47 -0300

    2era entrega

--- src/main/java/com/coderhouse/models/Producto.java
+++ src/main/java/com/coderhouse/models/Producto.java
@@ -3,6 +3,8 @@ package com.coderhouse.models;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
@@ -11,11 +13,13 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "Productos")
 public class Producto {
+
 	@Id // Primary Key
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
@@ -26,18 +30,21 @@ public class Producto {
 	
 	private double precio;
 
-	@ManyToMany(fetch =FetchType.EAGER)
+	@ManyToMany(fetch = FetchType.EAGER)
 	@JoinTable(
-				name ="Ventas", 
-				joinColumns = @JoinColumn(name= "producto_id"), 
-				inverseJoinColumns = @JoinColumn(name = "cliente_id")
-			)
+			name = "Ventas", 
+			joinColumns = @JoinColumn(name = "producto_id"), 
+			inverseJoinColumns = @JoinColumn(name = "cliente_id"))
+	@JsonIgnore
 	private List<Cliente> clientes = new ArrayList<>();
+
+	@ManyToOne(fetch = FetchType.EAGER)
+	private Categoria categoria;
 
 	public Producto() {
 		super();
 	}
-	
+
 	public Producto(String nombre, int stock, double precio) {
 		this();
 		this.nombre = nombre;
@@ -57,16 +64,12 @@ public class Producto {
 		return nombre;
 	}
 
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-	
-	public int getStock() {
-		return stock;
+	public Categoria getCategoria() {
+		return categoria;
 	}
 
-	public void setStock(int stock) {
-		this.stock = stock;
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
 	}
 
 	public List<Cliente> getClientes() {
@@ -75,6 +78,20 @@ public class Producto {
 
 	public void setClientes(List<Cliente> clientes) {
 		this.clientes = clientes;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+	
+	
+	
+	public int getStock() {
+		return stock;
+	}
+
+	public void setStock(int stock) {
+		this.stock = stock;
 	}
 
 	public double getPrecio() {
@@ -87,10 +104,7 @@ public class Producto {
 
 	@Override
 	public String toString() {
-		return "Producto [id=" + id + ", nombre=" + nombre + ", stock=" + stock + ", precio=" + precio + ", clientes="
-				+ clientes + "]";
+		return "Curso [id=" + id + ", nombre=" + nombre + ", clientes=" + clientes + "]";
 	}
-
-	
 
 }
